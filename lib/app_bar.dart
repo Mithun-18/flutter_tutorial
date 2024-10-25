6c73b0597fcd8efe1dadc4f290c9a7f2c28ce4d0
Mithun-18 <mithunpoojary180@gmail.com>
2024-10-25 19:45:43 +0530

    app bar

---
+++ lib/app_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double appbarHeight = 80.0;
+
+  const MyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      flexibleSpace: SizedBox(
+        height: appbarHeight,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              _renderLeading(),
+              _renderTitle(),
+              _renderAction(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _renderLeading() {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+        color: Colors.white, // Set a background color if needed
+        borderRadius:
+            BorderRadius.circular(12.0), // Optional: add border radius
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(211, 209, 216, 0.3),
+            offset: Offset(0, 2),
+            blurRadius: 4,
+            spreadRadius: 1,
+          )
+        ],
+      ),
+      child: const Icon(Icons.arrow_back_ios_rounded, size: 16.0),
+    );
+  }
+
+  Widget _renderTitle() {
+    return Expanded(
+      child: Image.asset(
+        "assets/logo.png",
+        width: 132.0,
+        height: 44.0,
+      ),
+    );
+  }
+
+  Widget _renderAction() {
+    return const CircleAvatar(
+      backgroundImage: AssetImage("assets/profile.jpg"),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appbarHeight);
+}
