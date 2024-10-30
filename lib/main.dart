88a1baa084c92cab0aecc42ee1968e5b94591795
Mithun-18 <mithunpoojary180@gmail.com>
2024-10-30 13:33:38 +0530

    designing bottom navigation is started

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:testflutter/app_bar.dart';
 import 'package:testflutter/temple%20view/booking_card.dart';
-import 'package:testflutter/temple%20view/community_card_lg.dart';
 import 'package:testflutter/temple%20view/my_temple_card.dart';
 import 'package:testflutter/verification_page.dart';
-// import 'package:testflutter/my_test_screen.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const Test(initialValue: 0),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+  final screens = [
+    Container(
+      color: Colors.white,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(color: Colors.amber)
+  ];
+  @override
   Widget build(BuildContext context) {
+    // final controller = Get.put(NavigationController());
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => const Scaffold(
-              appBar: MyAppBar(),
-              body: CommunityCardLg(),
+        '/': (context) => Scaffold(
+              appBar: const MyAppBar(),
+              body: screens[selectedIndex],
+              bottomNavigationBar: NavigationBar(
+                elevation: 0,
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (index) => setState(() {
+                  selectedIndex = index;
+                }),
+                destinations: const [
+                  NavigationDestination(
+                      icon: Icon(Icons.home_filled), label: 'Home'),
+                  NavigationDestination(
+                      icon: Icon(Icons.home_filled), label: 'Events'),
+                  NavigationDestination(
+                      icon: Icon(Icons.home_filled), label: 'Community'),
+                  NavigationDestination(
+                      icon: Icon(Icons.home_filled), label: 'Manage'),
+                ],
+              ),
             ),
         '/verification': (context) => const Scaffold(
               appBar: MyAppBar(),
@@ -62,8 +74,12 @@ class MyApp extends StatelessWidget {
       // home: const Scaffold(
       //   appBar: MyAppBar(),
       //   body: Registration(),
-      // ),
+      // )
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+// class NavigationController extends GetxController {
+//   final Rx<int> selectedIndex = 0.obs;
+// }
