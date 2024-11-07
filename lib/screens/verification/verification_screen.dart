87fb8ea48275e21e391a4f1848467dae5e86f4e4
Mithun-18 <mithunpoojary180@gmail.com>
2024-11-07 12:09:29 +0530

    verification screen modifield

--- lib/screens/verification/verification_screen.dart
+++ lib/screens/verification/verification_screen.dart
@@ -4,26 +4,51 @@ import 'package:testflutter/common/app_colors.dart';
 import 'package:testflutter/common/app_routes.dart';
 import 'package:testflutter/common/base_screen.dart';
 
-class VerificationScreen extends StatelessWidget {
+class VerificationScreen extends StatefulWidget {
   const VerificationScreen({super.key});
+
+  @override
+  State<VerificationScreen> createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  final List<TextEditingController> _controllers =
+      List.generate(4, (_) => TextEditingController());
+  final List<FocusNode> _focusNodes = List.generate(4, (_) => FocusNode());
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNodes[0].requestFocus(); // Set focus on first field initially
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose(); // Dispose controllers
+    }
+    for (var focusNode in _focusNodes) {
+      focusNode.dispose(); // Dispose focus nodes
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
       screen: SingleChildScrollView(
         child: Container(
-          color: AppColors.white,
           padding: const EdgeInsets.all(24.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 60.0),
+              const SizedBox(height: 72.0),
               _renderTitle(context),
               const SizedBox(height: 8.0),
               _renderDescription(context),
               const SizedBox(height: 32.0),
               _renderVerificationTextField(context),
-              const SizedBox(height: 48.0),
+              const SizedBox(height: 40.0),
               _renderResendCodeMssg(context),
             ],
           ),
@@ -45,12 +70,8 @@ class VerificationScreen extends StatelessWidget {
   Widget _renderVerificationTextField(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        _renderVeficationFeild(context),
-        _renderVeficationFeild(context),
-        _renderVeficationFeild(context),
-        _renderVeficationFeild(context)
-      ],
+      children:
+          List.generate(4, (index) => _renderVeficationFeild(context, index)),
     );
   }
 
@@ -75,21 +96,33 @@ class VerificationScreen extends StatelessWidget {
     ));
   }
 
-  Widget _renderVeficationFeild(BuildContext context) {
+  Widget _renderVeficationFeild(BuildContext context, int index) {
     return SizedBox(
         width: 64,
         height: 64,
         child: TextFormField(
           cursorColor: AppColors.salmon,
           style: Theme.of(context).textTheme.displayMedium,
-          onChanged: (value) =>
-              {if (value.length == 1) FocusScope.of(context).nextFocus()},
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              // Move focus to the next field if it's within bounds
+
+              if (index < _focusNodes.length - 1) {
+                _focusNodes[index + 1].requestFocus();
+              } else {
+                _focusNodes[index].unfocus(); // Unfocus on the last field
+                // Optionally, add code here to handle complete input
+              }
+            } else if (value.isEmpty && index > 0) {
+              // Move focus to the previous field if it's within bounds
+              _focusNodes[index - 1].requestFocus();
+            }
+          },
           decoration: InputDecoration(
-            border: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide:
-                    BorderSide(color: Color.fromRGBO(52, 168, 83, 1.0))),
-            contentPadding: const EdgeInsets.symmetric(vertical: 20),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.munsell),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
             focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(color: AppColors.fern),
                 borderRadius: const BorderRadius.all(Radius.circular(10))),
@@ -101,6 +134,8 @@ class VerificationScreen extends StatelessWidget {
             LengthLimitingTextInputFormatter(1),
             FilteringTextInputFormatter.digitsOnly
           ],
+          controller: _controllers[index],
+          focusNode: _focusNodes[index],
         ));
   }
 }
